16aff3f1414af08f301728cd606585b8a3a87997
sauravets <saurav.dubey.ets@gmail.com>
2022-01-28 19:47:14 +0530

    solving-errors

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-document.getElementById("search-btn").addEventListener("click",function searchImage(event){
+document.getElementById("search-btn").addEventListener("click", function (event){
     event.preventDefault();
-    let filter = document.getElementById("myinput").value.toUpperCase();
-    // let div = document.getElementById("pics");
-    let img = document.getElementsByTagName("img");
-    let i,dataKeyword,txtValue;
+    let filter,img,i,dataKeyword,txtValue;
+    filter = document.getElementById("myinput").value.toUpperCase();
+    
+    img = document.getElementsByClassName("image");
+
+    let btns=document.querySelectorAll('[data-keyword]');
+    console.log(img);
+     
     for(i = 0; i < img.length; i++){
-        dataKeyword = img[i].dataset("data-keyword")[0];
-        txtValue = dataKeyword.textContent || dataKeyword.innerText;
+        dataKeyword = img[i].dataset.keyword;
+        
+        // split into keywords
+        // match substring in each keyword
+        // if substring found in any keyword, this is match
+
         if(txtValue.indexOf(filter) > -1){
-            img[i].style.display = "";
+            img[i].style.display = "block";
         }
         else{
             img[i].style.display = "none";
-        }
+        } 
     }
-console.log(img);
+
+   /*  let searchKey = filter.toLowerCase();
+    result_html(images, searchKey); */
 });
+ /*  console.log(img[i].getAttribute("keyword")); */
