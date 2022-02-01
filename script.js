9d368149f4094b0b70a1d3abbfe582526130388e
sauravets <saurav.dubey.ets@gmail.com>
2022-02-01 14:17:44 +0530

    complete

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 document.getElementById("search-btn").addEventListener("click", function (event){
     event.preventDefault();
-    let filter,img,i,dataKeyword,txtValue;
-    filter = document.getElementById("myinput").value.toUpperCase();
-    
-    img = document.getElementsByClassName("image");
-
-    let btns=document.querySelectorAll('[data-keyword]');
-    console.log(img);
-     
-    for(i = 0; i < img.length; i++){
-        dataKeyword = img[i].dataset.keyword;
-        
-        // split into keywords
-        // match substring in each keyword
-        // if substring found in any keyword, this is match
-
-        if(txtValue.indexOf(filter) > -1){
-            img[i].style.display = "block";
+    let filter = document.getElementById("myinput").value.toLowerCase();
+    let img = document.getElementsByClassName("image");
+    for(let i = 0; i < img.length; i++){ 
+        let imgKeyword = img[i].getAttribute('data-keyword');
+        img[i].parentNode.style.display = "none";
+        if(imgKeyword.indexOf(filter) > -1){
+            img[i].parentNode.style.display = "block";
         }
-        else{
-            img[i].style.display = "none";
-        } 
     }
-
-   /*  let searchKey = filter.toLowerCase();
-    result_html(images, searchKey); */
 });
- /*  console.log(img[i].getAttribute("keyword")); */
+
+ 
